74f5dbefb9059b9629776682979f3f53b8b75dbf
zylx <549535805@qq.com>
2021-01-09 14:16:25 +0800

    左侧组件列表，增加按钮和图片组件

---
+++ src/customComponents/VButton/config.js
@@ -0,0 +1,22 @@
+export default {
+  component: 'v-button',
+  label: '按钮',
+  propValue: '按钮',
+  icon: 'el-icon-thumb',
+  animations: [],
+  events: {},
+  style: {
+    width: 100,
+    height: 34,
+    borderWidth: '',
+    borderColor: '',
+    borderRadius: '',
+    fontSize: 14,
+    fontWeight: 500,
+    lineHeight: '',
+    letterSpacing: 0,
+    textAlign: '',
+    color: '',
+    backgroundColor: ''
+  }
+}
---
+++ src/customComponents/VImage/config.js
@@ -0,0 +1,13 @@
+export default {
+  component: 'VImage',
+  label: '图片',
+  icon: 'el-icon-picture',
+  propValue: require('@/assets/title.jpg'),
+  animations: [],
+  events: {},
+  style: {
+    width: 300,
+    height: 200,
+    borderRadius: ''
+  }
+}
