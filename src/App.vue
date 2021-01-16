7945e0e7beabc63beb4f81ff2aff40a652a78423
zylx <549535805@qq.com>
2021-01-16 11:25:08 +0800

    修改右侧属性栏内容过长不出现滚动条问题，并为滚动条增加美化样式

--- src/App.vue
+++ src/App.vue
@@ -10,3 +10,30 @@ export default {
   name: 'App'
 }
 </script>
+
+<style lang="scss">
+// 1. -webkit-scrollbar-track：滚动条轨道
+// 2. -webkit-scrollbar：滚动条的宽度
+// 3. -webkit-scrollbar-thumb：滚动条滑块
+::-webkit-scrollbar-track {
+  background: rgba(0, 0, 0, 0.1);
+  -webkit-border-radius: 0;
+  -moz-border-radius: 0;
+  border-radius: 0;
+}
+
+::-webkit-scrollbar {
+  -webkit-appearance: none;
+  width: 10px;
+  height: 10px;
+}
+
+::-webkit-scrollbar-thumb {
+  -webkit-border-radius: 5px;
+  -moz-border-radius: 5px;
+  border-radius: 5px;
+  background: rgba(0, 0, 0, 0.15);
+  transition: color 0.2s ease-in-out;
+  cursor: pointer;
+}
+</style>
--- src/components/AttrList.vue
+++ src/components/AttrList.vue
@@ -1,82 +1,105 @@
 <template>
-    <div class="attr-list">
-        <el-form size="small" label-width="70px">
-            <el-form-item v-for="(key, index) in styleKeys" :key="index" :label="map[key]">
-                <el-color-picker v-if="key == 'borderColor'" v-model="curComponent.style[key]"></el-color-picker>
-                <el-color-picker v-else-if="key == 'color'" v-model="curComponent.style[key]"></el-color-picker>
-                <el-color-picker v-else-if="key == 'backgroundColor'" v-model="curComponent.style[key]"></el-color-picker>
-                <el-select v-else-if="key == 'textAlign'" v-model="curComponent.style[key]">
-                    <el-option
-                        v-for="item in options"
-                        :key="item.value"
-                        :label="item.label"
-                        :value="item.value"
-                    ></el-option>
-                </el-select>
-                <el-input type="number" v-else v-model="curComponent.style[key]" />
-            </el-form-item>
-            <el-form-item label="内容" v-if="curComponent && curComponent.propValue && !excludes.includes(curComponent.component)">
-                <el-input type="textarea" v-model="curComponent.propValue" />
-            </el-form-item>
-        </el-form>
-    </div>
+  <div class="attr-list">
+    <el-form size="small" label-width="70px">
+      <el-form-item
+        v-for="(key, index) in styleKeys"
+        :key="index"
+        :label="map[key]"
+      >
+        <el-color-picker
+          v-if="key == 'borderColor'"
+          v-model="curComponent.style[key]"
+        ></el-color-picker>
+        <el-color-picker
+          v-else-if="key == 'color'"
+          v-model="curComponent.style[key]"
+        ></el-color-picker>
+        <el-color-picker
+          v-else-if="key == 'backgroundColor'"
+          v-model="curComponent.style[key]"
+        ></el-color-picker>
+        <el-select
+          v-else-if="key == 'textAlign'"
+          v-model="curComponent.style[key]"
+        >
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
+        </el-select>
+        <el-input type="number" v-else v-model="curComponent.style[key]" />
+      </el-form-item>
+      <el-form-item
+        label="内容"
+        v-if="
+          curComponent &&
+          curComponent.propValue &&
+          !excludes.includes(curComponent.component)
+        "
+      >
+        <el-input type="textarea" v-model="curComponent.propValue" />
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            excludes: ['Picture'], // 这些组件不显示内容
-            options: [
-                {
-                    label: '左对齐',
-                    value: 'left',
-                },
-                {
-                    label: '居中',
-                    value: 'center',
-                },
-                {
-                    label: '右对齐',
-                    value: 'right',
-                },
-            ],
-            map: {
-                left: 'x 坐标',
-                top: 'y 坐标',
-                height: '高度',
-                width: '宽度',
-                color: '颜色',
-                backgroundColor: '背景色',
-                borderWidth: '边框宽度',
-                borderColor: '边框颜色',
-                borderRadius: '边框半径',
-                fontSize: '字体大小',
-                fontWeight: '字体粗细',
-                lineHeight: '行高',
-                letterSpacing: '字间距',
-                textAlign: '对齐方式',
-                opacity: '透明度',
-                rotate: '旋转角度',
-            },
-        }
-    },
-    computed: {
-        styleKeys() {
-            return this.$store.state.curComponent? Object.keys(this.$store.state.curComponent.style) : []
+  data () {
+    return {
+      excludes: ['v-image', 'chart-bar'], // 这些组件不显示内容
+      options: [
+        {
+          label: '左对齐',
+          value: 'left'
         },
-        curComponent() {
-            return this.$store.state.curComponent
+        {
+          label: '居中',
+          value: 'center'
         },
+        {
+          label: '右对齐',
+          value: 'right'
+        },
+      ],
+      map: {
+        left: 'x 坐标',
+        top: 'y 坐标',
+        height: '高度',
+        width: '宽度',
+        color: '颜色',
+        backgroundColor: '背景色',
+        borderWidth: '边框宽度',
+        borderColor: '边框颜色',
+        borderRadius: '边框半径',
+        fontSize: '字体大小',
+        fontWeight: '字体粗细',
+        lineHeight: '行高',
+        letterSpacing: '字间距',
+        textAlign: '对齐方式',
+        opacity: '透明度',
+        rotate: '旋转角度'
+      }
+    }
+  },
+  computed: {
+    styleKeys () {
+      return this.$store.state.curComponent ? Object.keys(this.$store.state.curComponent.style) : []
     },
+    curComponent () {
+      return this.$store.state.curComponent
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .attr-list {
-    height: 100%;
-    padding: 20px;
-    padding-top: 0;
-    overflow: auto;
+  position: absolute;
+  height: 100%;
+  padding: 20px;
+  overflow: auto;
 }
 </style>
